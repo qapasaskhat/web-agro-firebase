664e2e8d9a12f88264be76012c1cbd73111deb96
kapasaskhat <askhat@crontechs.com>
2024-09-10 12:13:57 +0500

    app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>BaiAgro</h3>
+        <a href='https://play.google.com/store/apps/details?id=com.agro.kz'>
+          Play Market
+        </a>
+        <a href='https://apps.apple.com/kz/app/baiagro/id1591945028'>
+          App Store
+        </a>
       </header>
     </div>
   );
